167cd76c603c0bc144cfa454c9602c09cc495e1b
Simao Gomes Viana <xdevs23@outlook.com>
2019-10-20 20:28:54 +0200

    Remove lineage stuff for now

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,7 +1,8 @@
 #
-# This policy configuration will be used by all products that
-# inherit from Lineage
+# This policy configuration will be used by all products
 #
+
+CUSTOM_DEVICE_SEPOLICY_PATH := device/custom/sepolicy
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
 ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
@@ -10,16 +11,16 @@ endif
 endif
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/public
+    $(CUSTOM_DEVICE_SEPOLICY_PATH)/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/private
+    $(CUSTOM_DEVICE_SEPOLICY_PATH)/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/dynamic
+    $(CUSTOM_DEVICE_SEPOLICY_PATH)/common/dynamic
 else
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/vendor
+    $(CUSTOM_DEVICE_SEPOLICY_PATH)/common/dynamic \
+    $(CUSTOM_DEVICE_SEPOLICY_PATH)/common/vendor
 endif
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,7 +1,8 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Lineage
 #
+
+CUSTOM_QCOM_DEVICE_SEPOLICY_PATH := device/custom/sepolicy/qcom
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
 ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
@@ -10,9 +11,9 @@ endif
 endif
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    $(CUSTOM_QCOM_DEVICE_SEPOLICY_PATH)/private
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/vendor
+    $(CUSTOM_QCOM_DEVICE_SEPOLICY_PATH)/qcom/vendor
 endif
